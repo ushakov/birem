10d7a7fda303c6aea6bb082e7f46b22d297d20fc
Max Ushakov <ushmax@gmail.com>
2009-10-03 22:50:31 +0400

    set up mail sending with throttled queue

--- send_mails.py
+++ send_mails.py
@@ -1,37 +1,82 @@
+#!/usr/bin/python
+
+import wsgiref.handlers
+
 from google.appengine.ext import webapp
 from google.appengine.ext import db
+from google.appengine.api import mail
 from google.appengine.api import users
+
+from google.appengine.api.labs import taskqueue
+
 import datetime
 
 import template
 import data
 
-_UUID = "526f04e1-8d3c-4001-987d-8f598100dde5"
+mailqueue = taskqueue.Queue("mail")
 
-remdb = data.ReminderDB()
-
-class Handler(webapp.RequestHandler):
+class DispatchHandler(webapp.RequestHandler):
     def get(self):
-        if ('key' not in self.request.params or
-            self.request.params != _UUID):
-            self.response.set_status(403)
+        request_params = {}
+        today = None
+        if self.request.params.get('date'):
+            param = self.request.params.get('date') 
+            today = datetime.datetime.strptime(param, "%d-%m-%Y").date()
+            request_params['date'] = param
+        users = data.remdb.UsersToRemind(today)
+        for user in users:
+            request_params['user'] = user.email()
+            task = taskqueue.Task(url="/mail/send", request_params)
+            mailqueue.add(task)
+
+
+class SendMailHandler(webapp.RequestHandler):
+    def post(self):
+        email = self.request.params('user')
+        if not email:
+            self.response.set_status("500")
+            return
+        try:
+            user = users.User(email)
+        except UserNotFoundError:
+            self.response.set_status("500")
             return
 
-        users = remdb.UsersToRemind()
-        for user in users:
-            tpl = {}
-            tpl['reminders'] = []
+        tpl = {}
+        tpl['reminders'] = []
+        if self.request.params.get('date'):
+            param = self.request.params.get('date') 
+            today = datetime.datetime.strptime(param, "%d-%m-%Y").date()
+        else:
             today = datetime.date.today()
-            tpl['date'] = {}
-            tpl['date']['day'] = today.day
-            tpl['date']['month'] = data.Months.Name(today.month)
-            tpl['date']['year'] = today.year
-            tpl['date']['weekday'] = today.weekday
+        tpl['date'] = {}
+        tpl['date']['day'] = today.day
+        tpl['date']['month'] = data.Months.Name(today.month)
+        tpl['date']['year'] = today.year
+        tpl['date']['weekday'] = data.Weekdays.Name(today.weekday)
+        
+        for rem in data.remdb.RemindersToSend(user, today):
+            if rem.day == today.day && rem.month == today.month:
+                tpl['today'] = rem.AsDict()
+            else:
+                tpl['reminders'].append(rem.AsDict())
 
-            for rem in remdb.RemindersToSend(user):
-                if rem.day == today.day && rem.month == today.month:
-                    tpl['today'] = rem.AsDict()
-                else:
-                    tpl['reminders'].append(rem.AsDict())
-            mail = template.Render("mail.html", tpl)
+        mail_text = template.Render("mail.tpl", tpl)
+        subject = template.Render("subject.tpl", tpl)
+        mail.send_mail("ushmax@gmail.com", user.email(),
+                       subject,
+                       mail_text)
             
+            
+urlmap = [('/mail/dispatch', DispatchHandler),
+          ('/mail/send', SendMailHandler)]
+
+def main():
+  application = webapp.WSGIApplication(urlmap,
+                                       debug=True)
+  wsgiref.handlers.CGIHandler().run(application)
+
+
+if __name__ == '__main__':
+  main()
